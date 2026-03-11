e8322235f22edbbb063ebb357553708edbeadd69
Ankit Gupta <ag5756042@gmail.com>
2026-03-12 00:45:26 +0530

    Remove user filter from getBySlug query
    Removed user filter from website query.

--- server/controllers/website.controller.js
+++ server/controllers/website.controller.js
@@ -369,7 +369,6 @@ export const getBySlug = async (req, res) => {
   try {
     const website = await Website.findOne({
       slug: req.params.slug,
-      user: req.user._id,
     });
     if (!website) {
       return res.status(400).json({ message: "Website not found" });
